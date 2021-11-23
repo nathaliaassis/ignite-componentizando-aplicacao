ad1faf4579d5e1bf3c9420d7bc20619dbd7cf47e
nathaliaassis <nathaliamassis@gmail.com>
2021-11-23 12:38:28 -0300

    first-commit

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,23 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from './MovieCard';
+
+interface ContentProps {
+  movies: any[];
+}
+
+export function Content({ movies }: ContentProps) {
+  return (
+    <main>
+      <div className="movies-list">
+        {movies.map(movie => (
+          <MovieCard
+            key={movie.imdbID}
+            title={movie.Title}
+            poster={movie.Poster}
+            runtime={movie.Runtime}
+            rating={movie.Ratings[0].Value}
+          />
+        ))}
+      </div>
+    </main>
+  )
 }
